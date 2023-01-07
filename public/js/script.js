a81400bb5772cb2aac2460d70a1491f155a72207
vodnibivol <vodnibivol@gmail.com>
2023-01-07 23:50:03 +0100

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,75 @@
+const $ = (sel) => document.querySelector(sel);
+const socket = io();
+
+const Bomba = {
+  roomName: new URLSearchParams(location.search).get('room'),
+  cells: new Array(9).fill(-1),
+  playerNo: -1, // 0 ali 1, določi server
+  playersNo: 1,
+  turn: -1,
+  msgAlert: false, // pojavi se za določen čas, če kaj narobe klikneš ipd...
+
+  get msg() {
+    if (this.playerNo === -1) return 'OBSERVER';
+
+    if (this.playersNo < 2) return 'WAITING FOR OTHER PLAYER...';
+    else if (this.winner !== null) return this.winner === this.playerNo ? 'YOU WON' : 'YOU LOST';
+    else return this.yourTurn ? 'YOUR TURN' : "OTHER PLAYER'S TURN";
+  },
+
+  get msgShown() {
+    return this.playerNo === -1 || this.playersNo < 2 || this.winner !== null || this.msgAlert;
+  },
+
+  get yourTurn() {
+    return this.turn !== -1 && this.turn === this.playerNo;
+  },
+
+  get disabled() {
+    if (this.playerNo === -1) return true;
+    return !(this.playersNo > 1 && this.winner === null && this.yourTurn);
+  },
+
+  init() {
+    socket.emit('ACCESS_ROOM', this.roomName);
+    socket.on('GRANT_ROOM_ACCESS', ({ playerNo, roomName }) => {
+      this.playerNo = playerNo;
+    });
+    socket.on('GAME_STATE', ({ cells, turn, playersNo }) => {
+      this.turn = turn;
+      this.cells = cells;
+      this.playersNo = playersNo;
+    });
+
+    // --- events
+    document.onkeydown = (e) => e.key === 'r' && socket.emit('RESET_GAME', this.roomName);
+    document.onclick = (e) => e.target.matches('#board, .cell') || this.showMsg();
+  },
+
+  get winner() {
+    const WINS = ['012', '345', '678', '036', '147', '258', '048', '246'];
+    let w = null;
+    [0, 1].forEach((playerNo) => {
+      if (WINS.some((combo) => [...combo].every((index) => this.cells[index] === playerNo))) w = playerNo;
+    });
+    return w;
+  },
+
+  onClick(index) {
+    if (this.playerNo === -1) return console.warn('OBSERVER');
+    if (this.cells[index] !== -1) return console.warn('CELL FULL');
+
+    this.cells[index] = this.playerNo;
+    this.turn = 1 - this.turn;
+
+    socket.emit('GAME_STATE', {
+      cells: this.cells,
+      turn: this.turn,
+      winner: this.winner,
+    });
+  },
+
+  showMsg() {
+    this.msgAlert = true && setTimeout(() => (this.msgAlert = false), 2000);
+  },
+};
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const ROOMS = {
+  data: {},
+  get(roomName) {
+    this.data[roomName] = this.data[roomName] || new Room(roomName);
+    return this.data[roomName];
+  },
+  set(roomName, room) {
+    this.data[roomName] = room;
+  },
+  clean() {
+    // remove room if no players .. NOT USED because of resetting ipd.
+    const keys = Object.keys(this.data);
+    for (let i = keys.length - 1; !!i; --i) {
+      const key = keys[i];
+      if (this.data[key].players[0] === null && this.data[key].players[1] === null) delete this.data[key];
+    }
+  },
+};
+
+class Room {
+  constructor(name, players) {
+    this.name = name;
+    this.players = players || [null, null];
+    this.turn = 0;
+    this.cells = new Array(9).fill(-1);
+    this.winner = null;
+  }
+
+  addPlayer(playerId) {
+    const freeSpot = this.players.indexOf(null);
+    if (freeSpot !== -1) this.players[freeSpot] = playerId;
+    return freeSpot;
+  }
+}
+
+function getState(roomName) {
+  const room = ROOMS.get(roomName);
+
+  return {
+    cells: room.cells,
+    turn: room.turn,
+    playersNo: room.players.filter((p) => !!p).length,
+  };
+}
+
+io.on('connection', (socket) => {
+  let ROOM_NAME;
+
+  socket.on('ACCESS_ROOM', (roomName) => {
+    ROOM_NAME = roomName;
+
+    const playerIndex = ROOMS.get(ROOM_NAME).addPlayer(socket.id);
+
+    socket.emit('GRANT_ROOM_ACCESS', { playerNo: playerIndex, roomName });
+    socket.join(ROOM_NAME);
+    io.to(ROOM_NAME).emit('GAME_STATE', getState(ROOM_NAME));
+  });
+
+  socket.on('GAME_STATE', ({ cells, turn, winner }) => {
+    ROOMS.get(ROOM_NAME).cells = cells;
+    ROOMS.get(ROOM_NAME).turn = turn;
+    ROOMS.get(ROOM_NAME).winner = winner;
+
+    io.to(ROOM_NAME).emit('GAME_STATE', getState(ROOM_NAME));
+  });
+
+  socket.on('RESET_GAME', (roomName) => {
+    ROOMS.set(ROOM_NAME, new Room(roomName, ROOMS.get(ROOM_NAME).players));
+    io.to(ROOM_NAME).emit('GAME_STATE', getState(ROOM_NAME));
+  });
+
+  socket.on('disconnect', () => {
+    ROOMS.get(ROOM_NAME).players = ROOMS.get(ROOM_NAME).players.map((p) => (p === socket.id ? null : p));
+    io.to(ROOM_NAME).emit('GAME_STATE', getState(ROOM_NAME));
+  });
+});
+
+server.listen(3000, () => console.log('listening on *:3000'));
